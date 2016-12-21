005b1bbf6703e855b8eb55470b6b8e2e1159b8f5
Dmitriy Anikin <dmitriyanikin1991@gmail.com>
2016-12-21 14:10:40 +0300

    Add test project in solution

---
+++ PasswordGenerator.Tests/PasswordGeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordGenerator.Tests
+{
+    [TestClass]
+    public class PasswordGeneratorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GenerateShouldRaiseExceptionIfStringIsNull()
+        {
+            //arrange
+
+            //act
+            var password = PasswordGenerator.Generate(null);
+
+            //assert
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        public static string Generate(string url)
+        {
+            if (url == null)
+                throw new ArgumentNullException();
+
+            var password = "";
+
+
+            return password;
+        }
+    }
+}
