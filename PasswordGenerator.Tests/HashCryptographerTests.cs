536068a9394171da6c4fd0fdcf9f32d93e8de0e3
Alexander Saraev <Alksar@yandex.ru>
2016-12-23 18:15:31 +0400

    Add HashCryptographer class that using dot net HashAlgorithm

---
+++ PasswordGenerator.Tests/HashCryptographerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace PasswordGenerator.Tests
+{
+    [TestFixture]
+    public class HashCryptographerTests
+    {
+        ICryptographer hashCryptographer;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            //arrange    
+            hashCryptographer = new HashCryptographer();            
+        }
+
+        [Test]
+        public void Check_instance_hashcryptographer()
+        {
+            //assert
+            Assert.That(hashCryptographer != null);
+        }
+
+        [Test]
+        public void Check_md5_as_default_cryptographer()
+        {
+            //arrange   
+            System.Security.Cryptography.HashAlgorithm md5 = new System.Security.Cryptography.MD5Cng();
+                       
+            //act
+            byte[] data = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes("lalala"));
+
+            System.Text.StringBuilder sBuilder = new System.Text.StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            string md5result = sBuilder.ToString();
+            
+            //assert
+            Assert.That(hashCryptographer.Encrypt("lalala") == md5result);
+        }
+    }
+}
---
+++ PasswordGenerator/HashCryptographer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PasswordGenerator
+{
+    public class HashCryptographer : ICryptographer
+    {
+        HashAlgorithm algorithm = new MD5Cng();
+
+        public string Encrypt(string input)
+        {
+            byte[] data = algorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            
+            return sBuilder.ToString();
+        }
+    }
+}
