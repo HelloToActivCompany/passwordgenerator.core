ffe762433dd06f956e4fe94bbfe213ae2f28991f
Alexander Saraev <Alksar@yandex.ru>
2016-12-26 23:12:42 +0400

    add test for HashCryptographer

--- PasswordGenerator.Tests/HashCryptographerTests.cs
+++ PasswordGenerator.Tests/HashCryptographerTests.cs
@@ -26,5 +26,18 @@ namespace PasswordGenerator.Tests
             //act + assert
             Assert.That(hashCryptographer.Encrypt("lalala") == "9aa6e5f2256c17d2d430b100032b997c");
         }
+
+        [Test]
+        public void Encrypt_length_of_result_hash_is_32()
+        {
+            //arrange
+            ICryptographer hashCryptographer = new HashCryptographer();
+
+            //act
+            string hash = hashCryptographer.Encrypt(@"https://msdn.microsoft.com/en-us/library/system.uri.iswellformeduristring(v=vs.110).aspx");
+
+            //assert
+            Assert.That(hash.Length == 32);
+        }
     }
 }
