d1d3a790ddb33183602ce9ee92c062849b4f125d
Dmitriy Anikin <dmitriyanikin1991@gmail.com>
2016-12-26 20:35:00 +0300

    Refact tests class

--- PasswordGenerator.Tests/HashCryptographerTests.cs
+++ PasswordGenerator.Tests/HashCryptographerTests.cs
@@ -7,25 +7,12 @@ namespace PasswordGenerator.Tests
     [TestFixture]
     public class HashCryptographerTests
     {
-        ICryptographer hashCryptographer;
-
-        [OneTimeSetUp]
-        public void Initialize()
-        {
-            //arrange    
-            hashCryptographer = new HashCryptographer();            
-        }
-
         [Test]
-        public void Check_instance_HashCryptographer()
+        public void Check_md5_as_default_cryptographer_without_param()
         {
-            //assert
-            Assert.That(hashCryptographer != null);
-        }
+            //arrange
+            ICryptographer hashCryptographer = new HashCryptographer();
 
-        [Test]
-        public void Check_md5_as_default_cryptographer()
-        {
             //act + assert
             Assert.That(hashCryptographer.Encrypt("lalala") == "9aa6e5f2256c17d2d430b100032b997c");
         }
