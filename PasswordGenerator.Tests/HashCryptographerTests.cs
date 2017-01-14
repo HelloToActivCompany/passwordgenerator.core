d35bbaf52696a984b5a142dfb7d4a4c861d880b7
Alexander Saraev <Alksar@yandex.ru>
2017-01-14 15:53:06 +0300

    add HashCryptographer constructor && constructor by default

--- PasswordGenerator.Tests/HashCryptographerTests.cs
+++ PasswordGenerator.Tests/HashCryptographerTests.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 using NUnit.Framework;
 using Moq;
+using PCLCrypto;
 
 namespace PasswordGenerator.Tests
 {
     [TestFixture]
     public class HashCryptographerTests
     {
-        [Test]
-        public void Encrypt_return_value_is_not_null_or_empty()
+        ICryptographer hashCryptographer;
+
+        [OneTimeSetUp]
+        public void Initialize()
         {
             //arrange
-            ICryptographer hashCryptographer = new HashCryptographer();
-            
+            hashCryptographer = new HashCryptographer();
+        }
+
+        [Test]
+        public void Encrypt_for_not_null_or_empty_param_return_value_is_not_null_or_empty()
+        {
             //act + assert
             Assert.That(!string.IsNullOrEmpty(hashCryptographer.Encrypt("default")));
+        }
+
+        [Test]
+        public void Constructor_by_default_is_Md5()
+        {
+            //arrange
+            ICryptographer md5Cryptographer = new HashCryptographer(HashAlgorithm.Md5);
+
+            //act + assert
+            Assert.That(hashCryptographer.Encrypt("") == md5Cryptographer.Encrypt(""));
         }
     }
 }
--- PasswordGeneratorCore/HashCryptographer.cs
+++ PasswordGeneratorCore/HashCryptographer.cs
@@ -5,10 +5,22 @@ namespace PasswordGenerator
 {
     public class HashCryptographer: ICryptographer
     {
+        private HashAlgorithm algorithm;
+
+        public HashCryptographer()
+        {
+            algorithm = HashAlgorithm.Md5;
+        }
+
+        public HashCryptographer(HashAlgorithm algorithm)
+        {
+            this.algorithm = algorithm;
+        }
+
         public string Encrypt(string input)
         {
             var data = System.Text.Encoding.UTF8.GetBytes(input);
-            var hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha1);
+            var hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(algorithm);
             var hash = hasher.HashData(data);
             var hashBase64 = Convert.ToBase64String(hash);
             return hashBase64;
