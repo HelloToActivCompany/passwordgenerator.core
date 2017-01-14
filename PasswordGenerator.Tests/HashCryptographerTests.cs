04839e5a6f73483f935e9d665bab570816619341
Dmitriy Anikin <dmitriyanikin1991@gmail.com>
2017-01-14 15:09:59 +0300

    Add HashCryptographer

---
+++ PasswordGenerator.Tests/HashCryptographerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace PasswordGenerator.Tests
+{
+    [TestFixture]
+    public class HashCryptographerTests
+    {
+        [Test]
+        public void Encrypt_return_value_is_not_null_or_empty()
+        {
+            //arrange
+            ICryptographer hashCryptographer = new HashCryptographer();
+            
+            //act + assert
+            Assert.That(!string.IsNullOrEmpty(hashCryptographer.Encrypt("default")));
+        }
+    }
+}
---
+++ PasswordGeneratorCore/HashCryptographer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PCLCrypto;
+
+namespace PasswordGenerator
+{
+    public class HashCryptographer: ICryptographer
+    {
+        public string Encrypt(string input)
+        {
+            var data = System.Text.Encoding.UTF8.GetBytes(input);
+            var hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha1);
+            var hash = hasher.HashData(data);
+            var hashBase64 = Convert.ToBase64String(hash);
+            return hashBase64;
+        }
+    }
+}
