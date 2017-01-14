80127073964c02e49bbfb3ffcd10d4f3cb10cf3e
Dmitriy Anikin <dmitriyanikin1991@gmail.com>
2017-01-14 18:05:12 +0300

    Refactoring

---
+++ PasswordGeneratorCore.Tests/HashCryptographerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using PasswordGeneratorCore;
+using PCLCrypto;
+
+namespace PasswordGeneratorCore.Tests
+{
+    [TestFixture]
+    public class HashCryptographerTests
+    {
+        ICryptographer _hashCryptographer;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            //arrange
+            _hashCryptographer = new HashCryptographer();
+        }
+
+        [Test]
+        public void Encrypt_for_not_null_or_empty_param_return_value_is_not_null_or_empty()
+        {
+            //act + assert
+            Assert.That(!string.IsNullOrEmpty(_hashCryptographer.Encrypt("default")));
+        }
+
+        [Test]
+        public void Constructor_by_default_is_Md5()
+        {
+            //arrange
+            ICryptographer md5Cryptographer = new HashCryptographer(HashAlgorithm.Md5);
+
+            //act + assert
+            Assert.That(_hashCryptographer.Encrypt("") == md5Cryptographer.Encrypt(""));
+        }
+    }
+}
---
+++ PasswordGeneratorCore.Tests/PasswordGeneratorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using PasswordGeneratorCore;
+
+namespace PasswordGeneratorCore.Tests
+{
+    [TestFixture]
+    public class PasswordGeneratorTests
+    {
+        IPasswordGenerator _generator;
+        string _key;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            //arrange            
+            var mock = new Mock<ICryptographer>();
+            mock.Setup(gen => gen.Encrypt(It.IsAny<string>())).Returns<string>(name => name);
+            _key = "supersecretkey";
+            _generator = new PasswordGeneratorCore.PasswordGenerator(mock.Object, _key);
+        }
+
+        [Test]
+        public void Check_instance_generator_with_cryptographer()
+        {
+            //assert
+            Assert.That(_generator != null);
+        }
+
+        [Test]        
+        public void Generate_should_raise_exception_if_string_is_null()
+        {
+            // act + assert
+            Assert.That(() => _generator.Generate(null),
+                Throws.TypeOf<ArgumentException>());            
+        }
+
+        [Test]        
+        public void Generate_should_raise_exception_if_string_is_empty()
+        {
+            // act + assert        
+            Assert.That(() => _generator.Generate(""),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Generate_should_create_password_for_url_as_hostname()
+        {
+            //act + assert
+            Assert.That(_generator.Generate(@"https://www.habrahabr.ru/post/150859/") == _key + "habrahabr.ru");
+        }
+
+        [Test]
+        public void Generate_should_create_password_for_email()
+        {
+            //act + assert
+            Assert.That(_generator.Generate(@"dmitriyanikin1991@gmail.com") == _key + "dmitriyanikin1991@gmail.com");
+        }
+    }
+}
