268cb5a984009b1b56c7c7b2c3a8d98b69a8402d
Dmitriy Anikin <dmitriyanikin1991@gmail.com>
2017-01-14 17:15:21 +0300

    Rafactoring

--- PasswordGeneratorCore/HashCryptographer.cs
+++ PasswordGeneratorCore/HashCryptographer.cs
@@ -5,23 +5,17 @@ namespace PasswordGeneratorCore
 {
     public class HashCryptographer: ICryptographer
     {
-        private readonly HashAlgorithm _algorithm;
-
-        public HashCryptographer()
+        private readonly IHashAlgorithmProvider _hasher;
+        
+        public HashCryptographer(HashAlgorithm algorithm = HashAlgorithm.Md5)
         {
-            _algorithm = HashAlgorithm.Md5;
-        }
-
-        public HashCryptographer(HashAlgorithm algorithm)
-        {
-            _algorithm = algorithm;
+            _hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(algorithm);
         }
 
         public string Encrypt(string input)
         {
             var data = System.Text.Encoding.UTF8.GetBytes(input);
-            var hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(_algorithm);
-            var hash = hasher.HashData(data);
+            var hash = _hasher.HashData(data);
             var hashBase64 = Convert.ToBase64String(hash);
             return hashBase64;
         }
