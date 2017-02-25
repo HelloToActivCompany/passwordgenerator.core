f5cc4f1f6c6252fd64d06bdeceb088d131f09912
Alexander <Alksar@yandex.ru>
2017-02-25 13:52:09 +0300

    Add IBytesToStringConvertable interface

---
+++ PasswordGenerator.Core/IBytesToStringConvertable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Core
+{
+    public interface IBytesToStringConvertable
+    {
+        string ConvertBytesToString(byte[] data);
+    }
+}
