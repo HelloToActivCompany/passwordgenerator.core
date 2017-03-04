d657b341208afe2069206fcb3eef4683d18c924d
Alexander <Alksar@yandex.ru>
2017-03-04 20:06:07 +0300

    refact

--- PasswordGenerator.Core/AlphabetHelper.cs
+++ PasswordGenerator.Core/AlphabetHelper.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace PasswordGenerator.Core
 {
     //TODO: move methods to the PasswordDescriptor
-    public static class AlphabetUtil
+    public static class AlphabetHelper
     {
         public static bool IsStringContainLowerCase(string str, Alphabet alphabet)
         {
